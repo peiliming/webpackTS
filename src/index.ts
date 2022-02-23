df6eb2fc2fba6cb315b3d52b08090f7b852971c0
 みんちゃん <ming@minchannoMacBook-Pro-2.local>
2022-02-23 13:32:37 +0900

    webpackv4.0環境初期構築

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Test from './test';
+
+const root = document.getElementById('root');
+const test = new Test('Hello World');
+test.sayHello(root);
---
+++ src/test.ts
@@ -0,0 +1,14 @@
+export default class Test {
+    message: string;
+
+    constructor(message: string) {
+        this.message = message;
+    }
+    
+    public sayHello(elem: HTMLElement | null) {
+        if (elem) {
+            elem.innerText = this.message;
+        }
+    }
+}
+
